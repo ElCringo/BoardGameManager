0b745d8a2cfb11d784318e308f0ebe804fa8f04d
ElCringo <kuba180693@gmail.com>
2025-07-14 16:46:16 +0200

    More properties added to the boardGames. Added the requirement for other properties and the related messages.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const router = new Router();
 app.use(bodyParser());
 app.use(router.routes());
 
-let boardGames = [{ id: 1, name: "SETI" }];
+let boardGames = [{ id: 1, name: "SETI", playersMin: 1, playersMax: 4, link: "https://boardgamegeek.com/boardgame/418059/seti-search-for-extraterrestrial-intelligence" }];
 
 // Neue API-Route hinzufügen: GET /board-games
 router.get("/board-games", (ctx) => {
@@ -18,11 +18,32 @@ router.get("/board-games", (ctx) => {
 
 router.post("/board-games", (ctx) => {
   const body = ctx.request.body;
-
+  /*NEXT: THE POSSIBILITY TO DELETE A BOARD GAME, A POSSIBILITY TO CHANGE A BOARD GAME.*/
   if (!body.name) {
     ctx.status = 400;
     ctx.body = {
-      message: "Please provide a name",
+      message: "Please provide a name of the game"
+    };
+    return;
+  }
+  if (!body.playersMin) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "Please provide a minimum number of players"
+    };
+    return;
+  }
+  if (!body.playersMax) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "Please provide a maximum number of players"
+    };
+    return;
+  }
+  if (!body.link) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "Please provide a link to the game"
     };
     return;
   }
